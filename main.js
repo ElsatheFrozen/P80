5031f317da9c163c0401fa7d83264df17c0f4525
ElsatheFrozen <81809525+ElsatheFrozen@users.noreply.github.com>
2021-06-03 18:03:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function Show()
 
 function search()
 {
-    var s=document.getElementById("names").value;
+    var s=document.getElementById("name").value;
     var found=0;
     var j;
     for(j=0; j<names_of_people.length; j++)
